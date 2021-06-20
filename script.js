97b58bba905d1391bb0eac9d8ba34568080c0b7e
Mipsmonsta <yaojintham@gmail.com>
2021-06-20 12:04:18 +0800

    near final draft

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const sharebar = document.body.querySelector(".sharebar");
 const avartarShareBtn = document.body.querySelector(".avatar__share");
+const avartar = document.body.querySelector(".avatar");
 let shareOpened = false;
 
 console.log(sharebar, avartarShareBtn);
@@ -7,10 +8,12 @@ console.log(sharebar, avartarShareBtn);
 avartarShareBtn.addEventListener("click", (event) => {
   if (shareOpened === false) {
     sharebar.classList.toggle("open");
+    avartar.style.backgroundColor = "hsl(217, 19%, 35%)"
     shareOpened = true;
   } else {
     sharebar.classList.toggle("open");
     shareOpened = false;
+    avartar.style.backgroundColor = "white";
   }
 
   event.stopPropagation(); //prevent bubbling up
